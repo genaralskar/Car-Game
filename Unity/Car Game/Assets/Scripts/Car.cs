1e85743d8e6659af00c07a0dbd998583248b416f
Collin <genaralskar1@gmail.com>
2019-03-14 21:55:53 -0600

    add ui mockup for car selection, start scripts for selection to work

---
+++ Unity/Car Game/Assets/Scripts/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace genaralskar
+{
+    /// <summary>
+    /// Contains information about a car, such as it's prefab, name, flavor text, and armors
+    /// </summary>
+    [CreateAssetMenu(menuName = "Car/New Car")]
+    public class Car : ScriptableObject
+    {
+        public GameObject carPrefab;
+        public string carName;
+        public string carFlavor;
+        public Armor[] armors;
+    }
+}
